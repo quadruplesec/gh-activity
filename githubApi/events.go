13eda074d1a9d8cb068f111c04885208491273bc
Gonçalo Santana <up202306919@up.pt>
2026-07-12 19:53:12 +0100

    added all commitcommentevent structs

--- githubApi/events.go
+++ githubApi/events.go
@@ -35,7 +35,40 @@ type CommitCommentEventPayload struct {
 }
 
 type CommitComment struct {
-	
+	Html_url string
+	Url string
+	Id int
+	Node_id string
+	Body string
+	Path string
+	Position int
+	Line int
+	Commit_Id string
+	Author_Association string
+	User User
+	Created_at string
+	Updated_at string
+}
+
+type User struct {
+	Login string
+	Id int
+	Node_id string
+	Avatar_url string
+	Gravatar_id string
+	Url string
+	Html_url string
+	Followers_url string
+	Following_url string
+	Gists_url string
+	Starred_url string
+	Subscriptions_url string
+	Organizations_url string
+	Repos_url string
+	Events_url string
+	Received_events_url string
+	Type string
+	Site_admin bool
 }
 
 type CreateEventPayload struct {
