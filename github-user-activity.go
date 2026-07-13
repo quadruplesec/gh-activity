710714505341cd0be0a8effcb042707eb99d4126
Gonçalo Santana <up202306919@up.pt>
2026-07-14 00:26:34 +0100

    made errors fatal

--- github-user-activity.go
+++ github-user-activity.go
@@ -13,31 +13,30 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		log.Println("Usage: ./github-user-activity <username>")	}
+		log.Fatal("Usage: ./github-user-activity <username>")
+	}
 
 	username := os.Args[1]
 	request := fmt.Sprintf("https://api.github.com/users/%s/events", username)
 
 	resp, err := http.Get(request)
 	if err != nil {
-		log.Println(err)
+		log.Fatalf("Network error: %v", err)
 	}
-
 	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
 
+	body, err := io.ReadAll(resp.Body)
 	var jsonResponse []githubapi.GithubEvent
 	err = json.Unmarshal(body, &jsonResponse)
 	if err != nil {
-		log.Printf("Error: %s", err)
+		log.Fatalf("Error: %s", err)
 	}
 
 	fmt.Printf("Recent Github Activity of %s:\n", username)
 	for i := 0; i < len(jsonResponse); i++ {
 		formatter, err := jsonResponse[i].UnmarshalEventPayload()
 		if err != nil {
-			fmt.Println(err)
-			return
+			log.Fatal(err)
 		}
 		fmt.Println(formatter.FormatActivity(
 			jsonResponse[i].Actor,
