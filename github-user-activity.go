8fc2d7880c40c985907eeb2ed660e642394402a0
Gonçalo Santana <up202306919@up.pt>
2026-07-13 23:59:30 +0100

    reformatted imports and variable declarations in main

--- github-user-activity.go
+++ github-user-activity.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"encoding/json"
-
 	"fmt"
-
 	"io"
-
 	"os"
-
 	"log"
-
 	"net/http"
 
 	"github.com/quadruplesec/github-user-activity/githubApi"
@@ -20,8 +15,8 @@ func main() {
 	if len(os.Args) != 2 {
 		log.Println("Usage: ./github-user-activity <username>")	}
 
-	var username string = os.Args[1]
-	var request string = fmt.Sprintf("https://api.github.com/users/%s/events", username)
+	username := os.Args[1]
+	request := fmt.Sprintf("https://api.github.com/users/%s/events", username)
 
 	resp, err := http.Get(request)
 	if err != nil {
