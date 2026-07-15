9ee6eb1f35d4b704c63148fd7d858570ed1b19d1
Gonçalo Santana <up202306919@up.pt>
2026-07-15 01:04:09 +0100

    implemented IsStale and LoadCache functions

--- github/cache.go
+++ github/cache.go
@@ -35,3 +35,29 @@ func SaveCache(activity ActivityEnvelope, username string) error {
 
 	return os.WriteFile(filePath, data, 0o600) // 0o600 -> rw-------
 }
+
+func IsStale(envelope ActivityEnvelope) bool {
+	return time.Since(envelope.FetchedAt) > envelope.TTL
+}
+
+func LoadCache(username string) (ActivityEnvelope, error) {
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return ActivityEnvelope{}, err
+	}
+	appCacheDir := filepath.Join(cacheDir, "gh-activity")
+	filePath := filepath.Join(appCacheDir, username+".json")
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return ActivityEnvelope{}, err
+	}
+
+	var envelope ActivityEnvelope
+	err = json.Unmarshal(data, &envelope)
+	if err != nil {
+		return ActivityEnvelope{}, err
+	}
+
+	return envelope, nil
+}
